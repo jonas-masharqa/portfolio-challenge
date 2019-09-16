07f446f7f7675a27e978c582a3d9f02fbb1a9c86
jonas <jonas.masharqa@gmail.com>
2019-09-16 22:24:49 +0200

    Adds webpack, Header and Footer
    Adds styling through Semantic ui and css directly in HTML.

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <nav className='ui fixed inverted menu'>
+            <div className="ui container">
+                <h3 className="header item">
+                    My portfolio
+                </h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
