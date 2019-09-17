488f93ea7a6308374c6d76c62cc068d8344a9bd7
jonas <jonas.masharqa@gmail.com>
2019-09-17 13:08:27 +0200

    Adds About and Projects.jsx with routes and links and in App

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => {
+    return (
+        <div className="ui container">
+            <h1 className="ui header">About Me</h1>
+            <p>A little about me</p>
+        </div>)
+    
+}
+
+export default About
---
+++ src/Projects.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Projects = () => {
+return (
+    <div className="ui container">
+        <h1 className="ui header">My Projects</h1>
+        <p>These are my projects</p>
+    </div>)
+}
+
+export default Projects
